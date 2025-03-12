dd9f2171ecb6f347bb10f18c4c30393e827e5df1
Gayathri-Musham-07 <145895648+Gayathri-Musham-07@users.noreply.github.com>
2025-03-12 16:36:40 +0530

    Update run-linter.sh

--- run-linter.sh
+++ run-linter.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Running ESLint..."
-npx eslint scriptt.js || echo "Linting finished with warnings"
+npx scriptt.js || echo "Linting finished with warnings"
 
 echo "Running Stylelint..."
 npx stylelint "**/*.css" || echo "Linting finished with warnings"
